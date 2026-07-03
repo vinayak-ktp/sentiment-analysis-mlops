6fb7014457f1cb2ecb987da84dfa799e1d762b86
Vinayak K <kvinayak2579@gmail.com>
2026-07-03 20:33:29 +0530

    chore: set up project structure with cookiecutter

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from sentiment-analysis-mlops!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An end-to-end, production-ready MLOps pipeline automating model training, deployment, and lifecycle monitoring.',
+    author='Vinayak K',
+    license='MIT',
+)
